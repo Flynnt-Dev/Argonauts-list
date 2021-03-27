82890363b955dbdba2c1c6bb12bbf90c7231a787
Alsseed <alsseed@outlook.fr>
2021-03-27 17:48:03 +0100

    Add Argonaut Model with Mongoose.Schema

---
+++ backend/models/argonaut.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const argonautSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            minlenght: 3,
+        }
+    },
+    {
+        timestamp: true,
+    }
+);
+
+const Argonaut = mongoose.model('Argonaut', argonautSchema);
+
+module.exports = User;
